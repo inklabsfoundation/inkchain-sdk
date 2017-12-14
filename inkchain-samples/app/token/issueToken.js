d56e41658f6c2e1ece8901c894d0f2caf2fd4701
wangh09 <wanghaoforever@126.com>
2017-12-14 18:37:02 +0800

    add issueToken interface

---
+++ inkchain-samples/app/token/issueToken.js
@@ -0,0 +1,8 @@
+/**
+ * Created by wangh09 on 2017/12/14.
+ */
+require('../config');
+let invokeHandler = require('../invoke-transaction');
+invokeHandler.invokeChaincodeAdmin(['peer1', 'peer2'],'mychannel','ascc','registerAndIssueToken',['INKToken','1000000000000000000000000000','18','411b6f8f24F28CaAFE514c16E11800167f8EBd89'],'admin','org1',null, null).then((result) =>{
+    console.log(result);
+});
