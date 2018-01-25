0966698066dcbb27c23b0ea520a92dfadf1435e1
wangh09 <wanghaoforever@126.com>
2018-01-25 11:32:29 +0800

    add blockhash to transaction querying response

--- inkchain-client/lib/BlockDecoder.js
+++ inkchain-client/lib/BlockDecoder.js
@@ -545,6 +545,7 @@ payload -- {}
 		var proto_processed_transaction = _transProto.ProcessedTransaction.decode(processed_transaction_bytes);
 		processed_transaction.validationCode = proto_processed_transaction.getValidationCode();
 		processed_transaction.transactionEnvelope = decodeBlockDataEnvelope(proto_processed_transaction.getTransactionEnvelope());
+		processed_transaction.blockHash = proto_processed_transaction.getBlockHash().toString('hex');
 		return processed_transaction;
 	}
 };
